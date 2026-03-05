f8b985ed040e6934da75a2bfd85c67c13583f220
Zvi Basser <zvickb2@gmail.com>
2026-03-04 19:50:47 -0500

    Fix stale deployed assets by using network-first SW strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cookie-designer-v3";
+const CACHE_NAME = "cookie-designer-v4";
 const ASSETS = [
   "./",
   "./index.html",
@@ -54,15 +54,14 @@ self.addEventListener("fetch", (event) => {
   }
 
   event.respondWith(
-    caches.match(req).then((cached) => {
-      if (cached) return cached;
-      return fetch(req).then((res) => {
+    fetch(req)
+      .then((res) => {
         if (res && res.status === 200 && res.type === "basic") {
           const copy = res.clone();
           caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
         }
         return res;
-      });
-    })
+      })
+      .catch(() => caches.match(req))
   );
 });
